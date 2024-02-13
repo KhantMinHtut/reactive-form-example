ec9b1a57c340a43255d00e4f0f9948b2cb3558f1
KhantMinHtut <khantmh2002@gmail.com>
2024-02-13 21:12:24 +0630

    reactive-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'reactive-form-assignment';
+export class AppComponent implements OnInit {
+  signupForm!: FormGroup;
+
+  forbiddenProjectname = ['Test'];
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      projectname: new FormControl(null, [
+        Validators.required,
+        this.forbiddenNames.bind(this),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      status: new FormControl('stable'),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } | null {
+    // if the value is not found, if returns -1;
+    if (
+      control.value &&
+      this.forbiddenProjectname.indexOf(control.value) !== -1
+    ) {
+      return { nameIsForbidden: true };
+    }
+    return null;
+  }
 }
